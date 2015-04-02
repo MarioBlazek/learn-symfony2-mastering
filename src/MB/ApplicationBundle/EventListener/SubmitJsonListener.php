175d786725d6ec763b018ef6bb7614d872d64875
Marek <blazek.mario@gmail.com>
2015-04-02 07:50:39 +0200

    Error handling for JSON added

--- src/MB/ApplicationBundle/EventListener/SubmitJsonListener.php
+++ src/MB/ApplicationBundle/EventListener/SubmitJsonListener.php
@@ -2,6 +2,8 @@
 
 namespace MB\ApplicationBundle\EventListener;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 
 class SubmitJsonListener
@@ -11,6 +13,14 @@ class SubmitJsonListener
         $request = $event->getRequest();
         $content = $request->getContent();
         $data = json_decode($content, true);
+
+        if ( JSON_ERROR_NONE !== json_last_error() ) {
+            $data = array('message' => 'Invalid or malformed JSON');
+            $response = new JsonResponse($data, Response::HTTP_BAD_REQUEST);
+            $event->setResponse($response);
+            $event->stopPropagation();
+        }
+
         $request->request->add($data ?: array());
     }
 }
